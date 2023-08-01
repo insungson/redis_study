1dbb7245e48233b45fadfc769c2e6cc0c19f9a4c
Sonny <libertines11@gmail.com>
2023-08-02 02:22:22 +0900

    add_list

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,7 +1,35 @@
 import type { CreateBidAttrs, Bid } from '$services/types';
+import { bidHistoryKey } from '$services/keys';
+import { client } from '$services/redis';
+import { DateTime } from 'luxon';
 
-export const createBid = async (attrs: CreateBidAttrs) => {};
+export const createBid = async (attrs: CreateBidAttrs) => {
+	const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
+
+	return client.rPush(bidHistoryKey(attrs.itemId), serialized);
+};
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
-	return [];
+	// 뒤에서부터 보여줘야 하기 떄문에.. 아래와 같이 시작은 -1 로 계산해서 처리해야 한다.
+	const startIndex = -1 * offset - count;
+	const endIndex = -1 - offset;
+
+	const range = await client.lRange(bidHistoryKey(itemId), startIndex, endIndex);
+
+	return range.map((bid) => deserializeHistory(bid));
+};
+
+// 가격:시간  을 만들어주는 serialize 함수이다.
+const serializeHistory = (amount: number, createdAt: number) => {
+	return `${amount}:${createdAt}`;
+};
+
+// 저장된 가격:시간  -> 데이터로 변경해주는 deserialize 함수이다.
+const deserializeHistory = (stored: string) => {
+	const [amount, createdAt] = stored.split(':');
+
+	return {
+		amount: parseFloat(amount),
+		createdAt: DateTime.fromMillis(parseInt(createdAt))
+	};
 };
--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -1,7 +1,12 @@
 import { client } from '$services/redis';
-import { itemsKey, itemsByViewsKey } from '$services/keys';
+import { itemsKey, itemsByViewsKey, itemsViewsKey } from '$services/keys';
 
 export const incrementView = async (itemId: string, userId: string) => {
+	const inserted = await client.pfAdd(itemsViewsKey(itemId), userId);
+	// items:views#${itemId}  에 대해 userId 를 추가한다!!
+
+	// 위의 inserted 에서 1로 리턴을 줄 경우에만 views 의 카운트를 증가시킨다!!
+	if (inserted) {
 	// 아래와 같이 pipeline 처리
 	return Promise.all([
 		// 기존 hash에 views 항목 1추가
@@ -9,4 +14,6 @@ export const incrementView = async (itemId: string, userId: string) => {
 		// sorted set 에서 해당 상품 1 추가
 		client.zIncrBy(itemsByViewsKey(), 1, itemId)
 	]);
+	}
+
 };
